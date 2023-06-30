152d36464581c07f7a22ef31efadc422227aca7f
Pramit Kumar Pal <pramitpalkumar@gmail.com>
2023-06-30 13:47:17 +0000

    added opamp lvs clear log

--- Layout_v2/lvs/opamp/ext_opamp.tcl
+++
@@ -1,6 +0,0 @@
-load opamp.mag
-select top cell
-extract all
-ext2spice lvs
-ext2spice -o opamp.ext.spc
-quit
---
+++ Layout_v2/lvs/opamp/lvs.sh
@@ -0,0 +1,3 @@
+netgen -batch lvs "opamp.ext.spice opamp" "opamp.sch.spice opamp" \
+		  "$PDK_ROOT/$PDK/libs.tech/netgen/${PDK}_setup.tcl" \
+		  comp.out
